074d0cf75a38084b581dacab24a110bded65b746
María Andrea <pericoandrea@gmail.com>
2020-08-19 05:30:18 -0500

    collage component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import Card from './Card';
+import Collage from './Collage';
 
 const App = () => (
   <div>
-    <Card
-      image="assets/imgs/annie-spratt-213059.png"
-      label="Technology"
-      title=" Lorem ipsum dolor sit amet, consectetur adipisicing elit"
-      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dolor ipsum facilis officiis harum ipsam quibusdam assumenda perferendis alias, iure vero, deserunt, ea dignissimos placeat mollitia. Molestiae incidunt eos dolore?"
-    />
+    <Collage />
   </div>
 );
 
---
+++ src/components/Collage.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import Card from './Card';
+import './styles/collage.scss';
+
+const Collage = () => (
+  <div className="collage">
+    <Card
+      image="assets/imgs/annie-spratt-213059.png"
+      label="Technology"
+      title=" Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dolor ipsum facilis officiis harum ipsam quibusdam assumenda perferendis alias, iure vero, deserunt, ea dignissimos placeat mollitia. Molestiae incidunt eos dolore?"
+    />
+    <Card
+      image="assets/imgs/annie-spratt-213059.png"
+      label="Technology"
+      title=" Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dolor ipsum facilis officiis harum ipsam quibusdam assumenda perferendis alias, iure vero, deserunt, ea dignissimos placeat mollitia. Molestiae incidunt eos dolore?"
+    />
+    <Card
+      image="assets/imgs/annie-spratt-213059.png"
+      label="Technology"
+      title=" Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dolor ipsum facilis officiis harum ipsam quibusdam assumenda perferendis alias, iure vero, deserunt, ea dignissimos placeat mollitia. Molestiae incidunt eos dolore?"
+    />
+    <Card
+      image="assets/imgs/annie-spratt-213059.png"
+      label="Technology"
+      title=" Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dolor ipsum facilis officiis harum ipsam quibusdam assumenda perferendis alias, iure vero, deserunt, ea dignissimos placeat mollitia. Molestiae incidunt eos dolore?"
+    />
+    <Card
+      image="assets/imgs/annie-spratt-213059.png"
+      label="Technology"
+      title=" Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dolor ipsum facilis officiis harum ipsam quibusdam assumenda perferendis alias, iure vero, deserunt, ea dignissimos placeat mollitia. Molestiae incidunt eos dolore?"
+    />
+    <Card
+      image="assets/imgs/annie-spratt-213059.png"
+      label="Technology"
+      title=" Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dolor ipsum facilis officiis harum ipsam quibusdam assumenda perferendis alias, iure vero, deserunt, ea dignissimos placeat mollitia. Molestiae incidunt eos dolore?"
+    />
+    <Card
+      image="assets/imgs/annie-spratt-213059.png"
+      label="Technology"
+      title=" Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dolor ipsum facilis officiis harum ipsam quibusdam assumenda perferendis alias, iure vero, deserunt, ea dignissimos placeat mollitia. Molestiae incidunt eos dolore?"
+    />
+    <Card
+      image="assets/imgs/annie-spratt-213059.png"
+      label="Technology"
+      title=" Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dolor ipsum facilis officiis harum ipsam quibusdam assumenda perferendis alias, iure vero, deserunt, ea dignissimos placeat mollitia. Molestiae incidunt eos dolore?"
+    />
+    <Card
+      image="assets/imgs/annie-spratt-213059.png"
+      label="Technology"
+      title=" Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dolor ipsum facilis officiis harum ipsam quibusdam assumenda perferendis alias, iure vero, deserunt, ea dignissimos placeat mollitia. Molestiae incidunt eos dolore?"
+    />
+  </div>
+);
+
+export default Collage;
