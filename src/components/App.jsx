21bb333a818143b3f58ec71da6b2642530c5a914
María Andrea <pericoandrea@gmail.com>
2020-08-19 04:56:38 -0500

    Card props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,12 @@ import Card from './Card';
 
 const App = () => (
   <div>
-    <Card />
+    <Card
+      image="assets/imgs/annie-spratt-213059.png"
+      label="Technology"
+      title=" Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dolor ipsum facilis officiis harum ipsam quibusdam assumenda perferendis alias, iure vero, deserunt, ea dignissimos placeat mollitia. Molestiae incidunt eos dolore?"
+    />
   </div>
 );
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import './styles/card.scss';
 
-const Card = () => (
-  <div className="card">
-    <div className="card__image">
-      <img src="assets/imgs/annie-spratt-213059.png" alt="Logo" />
-      <div className="card__label">Technology</div>
+const Card = props => {
+  const { image, label, title, description } = props;
+  return (
+    <div className="card">
+      <div className="card__image">
+        <img src={image} alt="Card" />
+        <div className="card__label">{label}</div>
+      </div>
+      <h3 className="card__title">{title}</h3>
+      <p className="card__description">{description}</p>
     </div>
-    <h3 className="card__title">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-    </h3>
-    <p className="card__description">
-      Molestiae tenetur dolor architecto mollitia culpa maxime hic similique
-      dolore nihil nostrum doloribus corporis ex, maiores repudiandae ad!
-      Aliquid provident qui distinctio.
-    </p>
-  </div>
-);
+  );
+};
 
 export default Card;
